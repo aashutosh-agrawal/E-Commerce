1fcb72688d417187775cc2d00fbcc5cfcd78723d
inbornhandsome <60094252+inbornhandsome@users.noreply.github.com>
2020-06-28 02:42:25 +0530

    Update server.php

--- server.php
+++ server.php
@@ -2,8 +2,8 @@
 //form data saved here
 $n = $_POST['name'];
 $u = $_POST['username'];
-$m = $_POST['mobno'];
 $e = $_POST['email'];
+$m = $_POST['mobileno'];
 $p = $_POST['password'];
 
 //database connection
@@ -16,9 +16,9 @@ $dbname="ecommerce";
 $con=new mysqli($hostname,$un,$pass,$dbname);
 
 //create sign up table
-//$user="create table Users(name varchar(40),username varchar(10) primary key,email varchar(25),mobno int(10),password varchar(30))";
+//$user="create table Users(name varchar(50),username varchar(30) primary key,email varchar(50),mobileno int(15),password varchar(20))";
 //create login table
-//$login="create table log(username varchar(10) primary key,password varchar(30))";
+//$login="create table login_details(username varchar(30) primary key,password varchar(20))";
 
 $sql_u = "SELECT * FROM users WHERE username='$u'";
 $res_u = mysqli_query($con,$sql_u);
@@ -29,11 +29,11 @@ header("location:signin.html");
 else
 {
 //inserting values
-$s1="insert into Users(name,username,email,mobno,password) values('$n','$u','$e',$m,'$p')";
+$s1="insert into Users(name,username,email,mobileno,password) values('$n','$u','$e',$m,'$p')";
 if($con->query($s1))
 {
 //inserting in login
- $s2="insert into log(username,password) values('$u','$p')";
+ $s2="insert into login_details(username,password) values('$u','$p')";
  $con->query($s2);
  header("location:womenwear.html");
 }
