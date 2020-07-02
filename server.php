079be3a7884df42a66f871599f457011ff8f5a38
inbornhandsome <60094252+inbornhandsome@users.noreply.github.com>
2020-07-02 12:11:19 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,41 +1,49 @@
 <?php
-//form data saved here
+session_start();
+if(!isset($_POST['checkreg']))
+{
+	header("location:signin.php");
+}
+//---------------------------------form data saved here---------------------------
 $n = $_POST['name'];
 $u = $_POST['username'];
 $e = $_POST['email'];
 $m = $_POST['mobileno'];
 $p = $_POST['password'];
 
-//database connection
-$hostname="127.0.0.1";
-$un="root";
-$pass="";
-$dbname="ecommerce";
+//----------------------------------------------------database connection-------------------
+include("database_connection.php");
 
-//connecting database using php
-$con=new mysqli($hostname,$un,$pass,$dbname);
+//----------------------------------------create sign up table--------------------------------
+//$user="create table Users(name varchar(50),username varchar(30) primary key,email varchar(50),mobileno varchar(15),password varchar(50))";
+//$con->query($user);
+//-------------------------------------------create login table------------------------------
+//$login="create table login_details(username varchar(30) primary key, password varchar(50));";
+//$con->query($login);
 
-//create sign up table
-//$user="create table Users(name varchar(50),username varchar(30) primary key,email varchar(50),mobileno int(15),password varchar(20))";
-//create login table
-//$login="create table login_details(username varchar(30) primary key,password varchar(20))";
 
+//---------------------------------------------existing user checking------------------------------
 $sql_u = "SELECT * FROM users WHERE username='$u'";
-$res_u = mysqli_query($con,$sql_u);
+$res_u = $con->query($sql_u);
 if (mysqli_num_rows($res_u) > 0) 
 {
-header("location:signin.html");
+	$msg="Username already taken!!";
+	$_SESSION['err']=$msg;
+	header("location:signin.php");
 }
 else
 {
-//inserting values
-$s1="insert into Users(name,username,email,mobileno,password) values('$n','$u','$e',$m,'$p')";
+//---------------------------------------encrypting password------------------------------------
+$ep = md5($p);
+//----------------------------------------inserting values---------------------------------------
+$s1="insert into Users(name,username,email,mobileno,password) values('$n','$u','$e','$m','$ep')";
 if($con->query($s1))
 {
-//inserting in login
- $s2="insert into login_details(username,password) values('$u','$p')";
+//---------------------------------------------inserting in login-----------------------------------------
+ $s2="insert into login_details(username,password) values('$u','$ep')";
  $con->query($s2);
- header("location:womenwear.html");
+ $_SESSION['user']=$u;
+ header("location:womenwear.php");
 }
 else
 {
