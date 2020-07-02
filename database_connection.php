cbebadea3dbe6394fc0d4f2cb040b473f0adab4e
inbornhandsome <60094252+inbornhandsome@users.noreply.github.com>
2020-07-03 00:34:25 +0530

    Add files via upload

---
+++ database_connection.php
@@ -0,0 +1,10 @@
+<?php
+//-----------------------------------------------database connection---------------------------------------
+$hostname="127.0.0.1";
+$un="root";
+$pass="";
+$dbname="ecommerce";
+
+//----------------------------------------connecting database using php----------------------------------------
+$con=new mysqli($hostname,$un,$pass,$dbname);
+?>
