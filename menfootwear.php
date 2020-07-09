8ce7fd8ef489ddbc39f9ff125ad61a0cd043199f
avanya080 <60094310+avanya080@users.noreply.github.com>
2020-07-09 22:36:55 +0530

    Add files via upload

---
+++ menfootwear.php
@@ -0,0 +1,346 @@
+ <!--HTML5 DECLARARTION-->
+  <!DOCTYPE>
+  <html lang="en" dir="ltr">
+    <head>
+      <!--all meta tags-->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <title>Men's Footwear</title>
+      <!--Bootstrap CSS and other CSS files-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!--Bootstrap JS and other JS files-->
+       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
+
+   </head>
+    <body>
+      <div class="container-fluid">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+
+          <a class="navbar-brand" href="#"> <img src="assets/k.svg" width="120" height="120" alt=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav mx-auto">
+                  <li class="nav-item active">
+                    <h1><strong> <a class="nav-link text-light" href="#">ExpressDeals <span class="sr-only">(current)</span></a></strong>  </h1>
+                  </li>
+
+              </ul>
+
+               <ul class="navbar-nav">
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <img src="assets/log.png" width="30" height="30"/>
+                          </a>
+                      </li>
+
+
+
+
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarNavDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <img src="assets/more.png" width="30" height="30"/>
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">My Profile</a>
+                            <a class="dropdown-item" href="#">Orders</a>
+                            <a class="dropdown-item" href="#">Notification</a>
+                             <a class="dropdown-item" href="#">Wishlist</a>
+                             <a class="dropdown-item" href="#">Logout</a>
+                      </li>
+
+
+
+
+
+
+
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <img src="assets/shopping-cart.png" width="30" height="30"/>
+                          </a>
+                      </li>
+
+
+                  </ul>
+
+
+
+          </div>
+
+      </nav>
+
+    <nav class="navbar navbar-light bg-dark justify-content-between ">
+      <a class="navbar-brand mx-auto text-light" href="#"><b>Men's Wear</b></a>
+      <a class="navbar-brand mx-auto text-light" href="#"><b>Women's Wear</b></a>
+      <a class="navbar-brand mx-auto text-light" href="#"><b>Kid's Wear<</b></a>
+      <a class="navbar-brand mx-auto text-light" href="#"><b>Women's FootWear</b></a>
+      <a class="navbar-brand mx-auto text-light" href="#"><b>Men's FootWear</b></a>
+      <form class="form-inline ">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="submit" class="btn btn-dark">
+          <img src="assets/search.png" width="30" height="30"/>
+        </button>
+        <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+      </form>
+    </nav>
+
+
+ <!-- <div class="container"> -->
+
+    <br>
+    <br>
+
+
+
+  <div class="alert alert-light" role="alert">
+
+
+
+
+  <div class="alert text-light" align="center" role="alert">
+    <h3 style="color:hsl(355, 44%, 22%)"><i><b>Loafers</b></i></h3>
+    </div>
+    <br>
+    <br>
+
+
+    <div class="row">
+      <div class="col-md-4">
+
+    <div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:25rem;">
+      <img class="card-img-top img-fluid img-thumbnail" src="assets/37.jpeg">
+      <div class="card-body">
+        <h5 class="card-title">Men Loafers For Men</h5>
+        <p class="card-text">TASSEL LOAFERS 734 FOREST TASSEL LOAFERS IN BURGUNDY BOX-CALF.</p>
+        <div class="container">
+		<form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc1p1sm" name="productId"> View Item </button>
+ 		  </form>
+          </div>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-md-4">
+    <div class="card mrcard text-white bg-dark mb-3 mx-auto" style="width: 15rem;height:25rem;">
+      <img class="card-img-top img-fluid img-thumbnail" src="assets/38.jpeg">
+      <div class="card-body">
+        <h5 class="card-title">Knoos Loafers For Men  (Brown)</h5>
+        <p class="card-text">Knoos Brown Synthetic Leather Casual Loafer Loafers For Men  (Brown).</p>
+        <div class="container">
+		<form action="product.php" method="POST">
+ 			<button class="btn btn-light" type="submit" value="mfwc1p2sm" name="productId"> View Item </button>
+ 		  </form>
+          </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-md-4">
+  <div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:25rem;">
+    <img class="card-img-top img-fluid img-thumbnail" src="assets/39.jpeg">
+    <div class="card-body">
+      <h5 class="card-title">Copper Loafers</h5>
+      <p class="card-text">Knoos Copper Synthetic Leather Casual Loafer Soft Footwear Brand Men's Casual Shoes.</p>
+      <div class="container">
+	  <form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc1p3sm" name="productId"> View Item </button>
+ 		  </form>
+        </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</div>
+
+
+
+<div class="alert alert-light" role="alert">
+
+<div class="alert text-light" align="center" role="alert">
+  <h3 style="color:hsl(355, 44%, 22%)"><i><b>Sneekers</b></i></h3>
+  </div>
+<br>
+
+
+
+<div class="row">
+  <div class="col-md-4">
+
+<div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:32rem;">
+  <a href="#"><img class="card-img-top img-fluid img-thumbnail" src="assets/40.jpeg" ></a>
+  <div class="card-body">
+    <h5 class="card-title">Casual sneakers shoes for men Sneakers For Men  (White)</h5>
+    <p class="card-text">Spring White Shoes Men Casual Shoes Male Sneakers Cool Street Men Shoes Brand Man Footwear KA793.</p>
+    <div class="container">
+	<form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc2p1sm" name="productId"> View Item </button>
+ 		  </form>
+      </div>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card mrcard text-white bg-dark mb-3 mx-auto" style="width: 15rem;height:32rem;">
+  <img class="card-img-top img-fluid img-thumbnail" src="assets/41.jpeg">
+  <div class="card-body">
+    <h5 class="card-title">Hotstyle Sneakers For Men  (Black)</h5>
+    <p class="card-text">CLYMB Casual Shoes Men Shark Design Bottom Platform Sneakers Men Mesh Breathable Shoes Male Mixed Colors Trainers Shoes for Mens The Best White Sneakers For Men.</p>
+    <div class="container">
+	<form action="product.php" method="POST">
+ 			<button class="btn btn-light" type="submit" value="mfwc2p2sm" name="productId"> View Item </button>
+ 		  </form>
+      </div>
+  </div>
+</div>
+</div>
+
+
+<div class="col-md-4">
+<div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:32rem;">
+<img class="card-img-top img-fluid img-thumbnail" src="assets/42.jpeg">
+<div class="card-body">
+  <h5 class="card-title">Lace-up Sneakers</h5>
+  <p class="card-text">pinstyle Outdoor Casual Light Weight Lace-up Sneakers Sneakers For Men Sneakers For Men Futurecraft 4D Sports Running Shoe Sneakers Men Casual Shoes Court Royale AC Genuine Leather Sneakers.</p>
+  <div class="container">
+  <form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc2p3sm" name="productId"> View Item </button>
+ 		  </form>
+    </div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+<div class="alert alert-light" role="alert">
+
+<div class="alert text-light" align="center" role="alert">
+  <h3 style="color:hsl(355, 44%, 22%)"><i><b>Boots</b></i></h3>
+  </div>
+<br>
+
+
+
+<div class="row">
+  <div class="col-md-4">
+
+<div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:35rem;">
+  <img class="card-img-top img-fluid img-thumbnail" src="assets/43.jpeg">
+  <div class="card-body">
+    <h5 class="card-title">Roadster Boots For Men  (Brown)</h5>
+    <p class="card-text">Vintage Style Boots Men Shoes Casual Fashion Zipper Motorcycle Boots Shoes </p>
+    <div class="container">
+	<form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc3p1sm" name="productId"> View Item </button>
+ 		  </form>
+      </div>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card mrcard text-white bg-dark mb-3 mx-auto" style="width: 15rem;height:35rem;">
+  <img class="card-img-top img-fluid img-thumbnail" src="assets/44.jpeg" >
+  <div class="card-body">
+    <h5 class="card-title">Unistar Jungle Boots</h5>
+    <p class="card-text">Oil Stain & Water Resistant- Extra Cushion InnerSole -Light Weight Trekking Shoes Boots For Men (OliveGreen) Boots For Men  (Multicolor).</p>
+    <div class="container">
+	<form action="product.php" method="POST">
+ 			<button class="btn btn-light" type="submit" value="mfwc3p2sm" name="productId"> View Item </button>
+ 		  </form>
+      </div>
+  </div>
+</div>
+</div>
+
+
+<div class="col-md-4">
+<div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:35rem;">
+<img class="card-img-top img-fluid img-thumbnail" src="assets/45.jpeg" >
+<div class="card-body">
+  <h5 class="card-title">Warrior By Liberty (Olive) </h5>
+  <p class="card-text"> Fashionable Retro Outdoor Casual High-Top Genuine Leather Boots Men Men''s Black Casual Ankle Lace-Up Boots</p>
+  <div class="container">
+  <form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc3p3sm" name="productId"> View Item </button>
+ 		  </form>
+    </div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+
+<div class="alert alert-light" role="alert">
+<div class="alert text-light" align="center" role="alert">
+  <h3 style="color:hsl(355, 44%, 22%)"><i><b>Floaters</b></i></h3>
+  </div>
+<br>
+<div class="row">
+  <div class="col-md-4">
+
+<div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:25rem;">
+  <img class="card-img-top img-fluid img-thumbnail" src="assets/46.jpeg">
+  <div class="card-body">
+    <h5 class="card-title">Sparx Men Olive, Black Sandal</h5>
+    <p class="card-text">Royal Flex Men Grey Sports Sandals.</p>
+    <div class="container">
+	<form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc4p1sm" name="productId"> View Item </button>
+ 		  </form>
+      </div>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card mrcard text-white bg-dark mb-3 mx-auto" style="width: 15rem;height:25rem;">
+  <img class="card-img-top img-fluid img-thumbnail" src="assets/47.jpeg" >
+  <div class="card-body">
+    <h5 class="card-title">Sparx Men Blue Sports Sandal</h5>
+    <p class="card-text">Stylish flloter sandal, are to maximize your fashion. </p>
+    <div class="container">
+	<form action="product.php" method="POST">
+ 			<button class="btn btn-light" type="submit" value="mfwc4p2sm" name="productId"> View Item </button>
+ 		  </form>
+      </div>
+  </div>
+</div>
+</div>
+
+
+<div class="col-md-4">
+<div class="card mrcard text-dark bg-light mb-3 mx-auto" style="width: 15rem;height:25rem;">
+<img class="card-img-top img-fluid img-thumbnail" src="assets/48.jpeg" >
+<div class="card-body">
+  <h5 class="card-title">Woodland Men Olive Sandal</h5>
+  <p class="card-text">Pu Rocks Men Sandels Men's PU Brown Casual Outdoor Sandals and Floaters.</p>
+  <div class="container">
+  <form action="product.php" method="POST">
+ 			<button class="btn btn-dark" type="submit" value="mfwc4p3sm" name="productId"> View Item </button>
+ 		  </form>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+      <!-- </div> -->
+    </body>
+ </html>
